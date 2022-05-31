1c102c881f5cb92f065b251ac9b6888887ae9d07
Pavel Krusek <pavel.krusek@gmail.com>
2022-05-31 23:23:51 +0200

    + plugin for leaflet imports.

---
+++ plugins/leaflet-plugin.client.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import { LMap, LMarker, LTileLayer } from 'vue2-leaflet'
+
+Vue.component('LMap', LMap)
+Vue.component('LTileLayer', LTileLayer)
+Vue.component('LMarker', LMarker)
