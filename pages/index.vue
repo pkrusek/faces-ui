23427e5402147c8d43e46b78c9cd9c010bdd0478
Pavel Krusek <pavel.krusek@gmail.com>
2022-05-31 17:18:54 +0200

    + Firestore faces implementation - test.

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,43 @@
 <template>
-  <Tutorial />
+  <div>
+    <h1>Faces</h1>
+    <div v-for="face in faces" :key="face.id">
+      {{face.name}}
+    </div>
+  </div>
 </template>
 
 <script>
+// import firebase from 'firebase/app'
+// import 'firebase/firestore'
+
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      faces: [],
+    }
+  },
+  created() {
+    this.getFaces()
+  },
+  methods: {
+    async getFaces() {
+      const ref = this.$fire.firestore.collection('faces')
+      try {
+        const { docs } = await ref.get()
+
+        this.faces = docs.map(doc => {
+          const { id } = doc
+          const data = doc.data()
+          return { id, ...data }
+        })
+
+        console.log('Loaded faces', this.faces)
+      } catch (error) {
+        throw new Error('Something gone wrong!')
+      }
+    },
+  },
 }
 </script>
